2e341b9bfd696f41e83496b11251a7ab88b764cd
hiro_yamada <ashitaka0789@gmail.com>
2019-02-04 21:24:36 +0900

    GoForwardUnityChan 課題
    Cube が UnityChan 以外のものと衝突した時に効果音が鳴るようにしました

---
+++ Assets/CubeController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+
+    private float speed = -0.2f;
+    private float deadLine = -10;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(this.speed, 0, 0);
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        GetComponent<AudioSource>().Play();
+        if (other.gameObject.tag == "UnityChan" )
+        {
+            GetComponent<AudioSource>().Stop();
+        }
+    }
+}
